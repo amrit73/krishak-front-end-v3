1d83f493c338a4999d7c9d09dd146edd026aed98
BishalRaj <bisal1shakya@gmail.com>
2019-10-21 23:33:46 +0545

    signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-signup',
@@ -11,5 +12,56 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  
+registerUser(name: any,username: any,email: any,phone: any,location: any,company: any,password: any){
+  let base_url='http://localhost:90/'
+  let nameValue = name.value;
+    let emailValue = email.value;
+    let locationValue = location.value;
+    let usernameValue=username.value;
+    let phoneValue=phone.value;
+    let companyValue=company.value;
+    let passwordValue=password.value;
+
+    let data= {
+      name:nameValue,
+      email:emailValue,
+      location:locationValue,
+      userName:usernameValue,
+      password:passwordValue,
+      phone:phoneValue,
+      company:companyValue
+    }
+
+    console.log(data)
+    // alert('I am here')
+
+    if (nameValue == '') {
+      alert('Please enter a name *');
+    }
+    else if (emailValue=='') {
+     alert('Please enter a valid email');
+    }
+    else if (usernameValue=='') {
+     alert('Please enter a valid username');
+    }
+    else if (passwordValue=='') {
+     alert('Please enter a valid password');
+    }
+    else{
+      $.ajax({
+        type: 'POST',
+        url: base_url + 'register',
+        data,
+        success: function () {
+          alert('Success')
+        },
+        error: function () {
+          alert('error')
+        }
+      });
+    }
+}
 
 }
