ee851da5de3afe9cccdc5221d6bb058f3260acb2
BishalRaj <bisal1shakya@gmail.com>
2019-10-31 23:08:00 +0545

    to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProductsComponent } from './products/products.component';
-
-import { AddProductsComponent } from './add-products/add-products.component';
 import { CartComponent } from './cart/cart.component';
 import { FilteredproductsComponent } from './filteredproducts/filteredproducts.component';
 import { AddProductsComponent } from './add-products/add-products.component';
