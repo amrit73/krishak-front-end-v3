62772f7cc909397303d97672cd47d95ec4b5ec8b
BishalRaj <bisal1shakya@gmail.com>
2019-11-03 14:24:03 +0545

    test

--- e2e/src/signup.po.ts
+++ e2e/src/signup.po.ts
@@ -6,17 +6,32 @@ export class SignupPage {
     return browser.get(this.test) as Promise<any>;
   }
 
-  
+  setName() {
+    return element(by.css('#name')).sendKeys('amrit shrestha');
+  }
 
   setUsername() {
-    return element(by.css('#username')).sendKeys('amrit@123gmail.com');
+    return element(by.css('#username')).sendKeys('amrit');
   }
+
+  setEmail() {
+    return element(by.css('#email')).sendKeys('amrit@123gmail.com');
+  }
+
+  setPhone() {
+    return element(by.css('#phone')).sendKeys('9813605819');
+  }
+
+  setLocation() {
+    return element(by.css('#location')).sendKeys('Kathmandu');
+  }
+
 
   setPassword(){
     return element(by.css('#password')).sendKeys('abcd');
   }
 
   clickLogin(){
-    return element(by.css('#btn-login')).click();
+    return element(by.css('#btn-signup')).click();
   }
 } 
